ea5dc6e721f7aea59e36fcb8d015145dcded1ac6
diegomillandev <diegomillandev@gmail.com>
2025-10-05 19:14:10 -0500

    fix: error redirecto correct

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   serverExternalPackages: ["@node-rs/argon2"],
+  output: "standalone",
 };
 
 export default nextConfig;
